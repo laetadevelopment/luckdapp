faeb042e00bed1d06c3bdb3df1593cbcee9582c7
Mike Laeta <mike@laetadevelopment.com>
2022-05-26 08:40:01 -0600

    t1.2.2
    finish initial smart contract test

--- test/LuckDApp.js
+++ test/LuckDApp.js
@@ -1,4 +1,5 @@
 const LuckDApp = artifacts.require("LuckDApp");
+const utils = require("./helpers/utils");
 const playerNames = ["Alice", "Bob"];
 contract("LuckDApp", (accounts) => {
   let alice = accounts[1];
@@ -8,21 +9,22 @@ contract("LuckDApp", (accounts) => {
     contractInstance = await LuckDApp.new();
   });
   it("should be able to create a new player", async () => {
-    const result = await contractInstance.newPlayer(playerNames[0], {from: alice});
-    assert.equal(result.receipt.status, true);
+    const player = await contractInstance.newPlayer(playerNames[0], {from: alice});
+    assert.equal(player.receipt.status, true);
   })
   it("should not be able to create two players", async () => {
-    const result = await contractInstance.newPlayer(playerNames[0], {from: alice});
-    assert.equal(result.receipt.status, true);
+    await contractInstance.newPlayer(playerNames[0], {from: alice});
+    await utils.shouldThrow(contractInstance.newPlayer(playerNames[1], {from: alice}))
   })
   it("should be able to run Luck Challenge", async () => {
-    const playerLDNA = await contractInstance.playerLDNA(alice);
-    // console.log(playerLDNA);
-    // const results = await contractInstance.getPastEvents("LuckChallengeResults");
-    // if (results[0].returnValues.results) {
-    //   assert.equal(results[0].returnValues.results, true);
-    // } else {
-    //   assert.equal(results[0].returnValues.results, false);
-    // }
+    const player = await contractInstance.newPlayer(playerNames[0], {from: alice});
+    const playerLDNA = await contractInstance.playerLDNA.call(alice);
+    const challenge = await contractInstance.luckChallenge(playerLDNA[0], {from: alice});
+    const results = await contractInstance.getPastEvents("LuckChallengeResults");
+    if (results[0].returnValues.results) {
+      assert.equal(results[0].returnValues.results, true);
+    } else {
+      assert.equal(results[0].returnValues.results, false);
+    }
   })
 })
