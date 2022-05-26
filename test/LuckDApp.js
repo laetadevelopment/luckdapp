b36c8992be8f57208ebb5b68de8413634dd907b9
Mike Laeta <mike@laetadevelopment.com>
2022-05-26 09:23:25 -0600

    t1.2.2
    add two more test cases to smart contract test

--- test/LuckDApp.js
+++ test/LuckDApp.js
@@ -19,14 +19,32 @@ contract("LuckDApp", (accounts) => {
     await utils.shouldThrow(contractInstance.newPlayer(playerNames[1], {from: alice}))
   })
   it("should be able to run Luck Challenge", async () => {
-    const player = await contractInstance.newPlayer(playerNames[0], {from: alice});
+    await contractInstance.newPlayer(playerNames[0], {from: alice});
     const playerLDNA = await contractInstance.playerLDNA.call(alice);
     const challenge = await contractInstance.luckChallenge(playerLDNA[0], {from: alice});
     const results = await contractInstance.getPastEvents("LuckChallengeResults");
+    assert.equal(challenge.receipt.status, true);
     if (results[0].returnValues.results) {
       assert.equal(results[0].returnValues.results, true);
     } else {
       assert.equal(results[0].returnValues.results, false);
+    }
+  })
+  it("should not be able to purchase LDNA if player has LDNA", async () => {
+    await contractInstance.newPlayer(playerNames[0], {from: alice});
+    await utils.shouldThrow(contractInstance.getLDNA({from: alice, value: 5000000000000000}));
+  })
+  it("should be able to purchase one more LDNA if player has zero LDNA", async () => {
+    await contractInstance.newPlayer(playerNames[0], {from: alice});
+    const playerLDNA = await contractInstance.playerLDNA.call(alice);
+    await contractInstance.luckChallenge(playerLDNA[0], {from: alice});
+    const results = await contractInstance.getPastEvents("LuckChallengeResults");
+    if (results[0].returnValues.results) {
+      // TODO: need to make an efficient way to ensure player gets to zero LDNA to fully verify
+      await utils.shouldThrow(contractInstance.getLDNA({from: alice, value: 5000000000000000}));
+    } else {
+      const ldna = await contractInstance.getLDNA({from: alice, value: 5000000000000000});
+      assert.equal(ldna.receipt.status, true);
     }
   })
 })
